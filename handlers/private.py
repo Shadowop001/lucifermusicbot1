f7cd70312fddc95dcdc0c52889007369724559a8
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-03-27 10:29:36 -0700

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,7 +7,7 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""I am an open-source @CallsMusic bot, I let you play music in your group's voice chat.
+        f"""I let you play music in your group's voice chat.
 
 The commands I currently support are:
 
@@ -18,15 +18,17 @@ The commands I currently support are:
 /mute - mute the userbot
 /unmute - unmute the userbot
 /stop - clear the queue and remove the userbot from the call
+
+Also Add this [Music Bot Assistant](t.me/VCPlayRobot).
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Group", url="https://t.me/callsmusicchat"
+                        "Group", url="https://t.me/LaylaSupport"
                     ),
                     InlineKeyboardButton(
-                        "Channel", url="https://t.me/callsmusic"
+                        "Channel", url="https://t.me/LaylaList"
                     )
                 ]
             ]
